8cc93f1d260c7f963eeb9eb3cdfe1d8987b6e8fd
Dhruv Kulgod <dhruv.kulgod@gmail.com>
2023-02-05 15:09:31 -0600

    Added 4bit adder files

---
+++ Lab3/src/ripple_adder_4bit.sv
@@ -0,0 +1,16 @@
+module ripple_adder_4bit (
+	input  [3:0] A, B,
+	input         cin,
+	output [3:0] S,
+	output        cout
+);
+
+    /* TODO
+     *
+     * Insert code here to implement a 4 bit ripple adder.
+	  * To be used for 16 bit ripple adder and carry-select adder
+     * Your code should be completly combinational (don't use always_ff or always_latch).
+     * Feel free to create sub-modules or other files. */
+
+     
+endmodule
