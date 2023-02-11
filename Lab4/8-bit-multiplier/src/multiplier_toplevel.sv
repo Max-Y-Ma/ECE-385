1336dc343117aacf081332bff2098e18a90efb89
Max Ma <maxma2@illinois.edu>
2023-02-10 23:01:33 -0600

    Add Lab 4 Materials

---
+++ Lab4/8-bit-multiplier/src/multiplier_toplevel.sv
@@ -0,0 +1,17 @@
+module multiplier_toplevel (
+	input  logic			Clk, 
+								Reset_Load_Clear, 
+								Run,
+	input  logic [7:0]	SW,
+	output logic [6:0]	HEX0, 
+								HEX1, 
+								HEX2, 
+								HEX3,
+	output logic [7:0]   Aval, 
+								Bval,
+	output logic 			Xval
+);
+
+// Top Level Module
+
+endmodule
